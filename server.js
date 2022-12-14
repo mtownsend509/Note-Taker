0d9392c6e9c057562d50dd90336de9ae56c2c9be
mtownsend509 <mgtownsend509@gmail.com>
2022-12-13 19:58:59 -0800

    delete maybe works?

--- server.js
+++ server.js
@@ -6,28 +6,24 @@ const PORT = 3001;
 
 const path = require('path');
 
-//importing the json files that has the notes
-//const notes = require("note pathway goes here");
-// const requestedNotes = req.params
-
-// //sets up express to handle data parsing?
+// //sets up express to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+//sends the notes html page with the /notes param
 app.get("/notes", (req, res) => {
     res.sendFile(path.join(__dirname, './public/notes.html'));
 });
 
-
+//gives the notes list with the getNotes function
 app.get("/api/notes", (req, res) => {
    const notes = require('./db/db.json');
-   console.log(notes.length);
    console.log('notes getten')
    return res.json(notes)
 })
 
+//adds the notes to the note list with a random ID#
 app.post("/api/notes", (req, res) => {
-    console.log(req.body);
     const { title, text} = req.body;
     
     const newNote = {
@@ -52,19 +48,30 @@ app.post("/api/notes", (req, res) => {
     return res.json(newNote);
 });
 
+//hopefully splices out the notes from the notes list based on ID match
+app.delete('/api/notes/:id', (req, res) => {
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+        if (err) {
+            console.log(err)
+        } else {
+            const parsedData = JSON.parse(data);
+            for(i=0; i<parsedData.length; i++) {
+                if (req.params.id == parsedData[i].id) {
+                      parsedData.splice(i, 1);
+                      fs.writeFile(`./db/db.json`, JSON.stringify(parsedData, null, 2), (err) => 
+                     err
+                    ? console.error(err)
+                    :console.log('new note posted!'));
+                } else {
+                    console.log('nope');
+                }
+            }
+        }
+    })
+});
 
-
-// //defaults to index.jtml?
+// //defaults to index.html
 app.use(express.static('./public'))
-
-
-// app.get("/send", (req, res) => res.sendFile(path.join(__dirname, "public/send.html")));
-
-// app.get("/api", (req, res) => {
-//     res.json(notes) //after the notes are saved
-// })
-
-// res.json() //sends stuff as a json object
 
 app.listen(PORT, () =>
     console.log('Server is up and listening')
